1d88e7acd5118fd6f85637e0a2602b377f31f8d2
dpcshowa <1049311551@qq.com>
2018-08-09 16:12:31 +0800

    shan chu jd xiang mu

--- day68-solr/src/main/java/com/show/dao/ItemDao.java
+++
@@ -1,9 +0,0 @@
-package com.show.dao;
-
-import com.show.pojo.PageMsg;
-import org.apache.solr.client.solrj.SolrQuery;
-
-public interface ItemDao {
-
-    PageMsg qurryIndex(SolrQuery solrQuery);
-}
--- day68-solr/src/main/java/com/show/service/ItemService.java
+++
@@ -1,8 +0,0 @@
-package com.show.service;
-
-import com.show.pojo.PageMsg;
-
-public interface ItemService {
-
-    PageMsg queryIndex(String queryString, String catalog_name, String price, int curPage, int sort, int rows);
-}
